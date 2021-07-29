dda7a88c3090c0563c88a577cdc02e586b516be6
dianjixz <18637716021@163.com>
2021-07-29 16:00:01 +0800

    [fix]

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # MaixPy3_scripts
+
+
+[MaixPy3 Scripts](https://github.com/sipeed/MaixPy3_scripts)
+====
+
+
+Scripts for [MaixPy3](https://github.com/sipeed/MaixPy3) ~ have a good time~
+
+Doc of MaixPy3: [maixpy3.sipeed.com](https://maixpy3.sipeed.com)
+
+## Directory Structure
+
+| directory | description |
+| --------- | ----------- |
+| application | Some complex applications |
+| basic | basic MaixPy3 usage |
+| x86 | x86 test code |
+| hardware | MaixPy3 API usage demo |
+| machine_vision | demo for vision related, including machine vision and AI processing related |
+
+
+-------------------------------
+
+运行在 [MaixPy3](https://github.com/sipeed/MaixPy3) 上的脚本，玩得开心～
+
+MaixPy 文档: [maixpy3.sipeed.com](https://maixpy3.sipeed.com)
+
+另外，国内也可以在[gitee](https://gitee.com/Sipeed/maixpy3_scripts)上查看，会实时同步, 对脚本有疑问请到[github](https://github.com/sipeed/MaixPy3_scripts/issues)搜索问题或者提问， MaixPy 固件相关问题请到[MaixPy issue](https://github.com/sipeed/MaixPy3_scripts/issues)提问
+
+
+
+## 目录结构
+
+| 目录 | 描述 |
+| --------- | ----------- |
+| application | 一些在v831上使用的python3包 |
+| basic | 基本的 maixpy3 使用 |
+| x86 | 在x86机器上测试 |
+| hardware | MaixPy3 API 使用例程 |
+| machine_vision | 视觉处理相关,包括机器视觉以及AI处理 |
+
--- examples.py
+++ examples.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from maix import camera
 from PIL import Image, ImageFont, ImageDraw
 from maix import display
 from maix import nn
@@ -12,7 +13,6 @@ import socket
 from maix import image
 import zbarlight
 from maix.nn import decoder
-
 
 def get_host_ip():
     try:
@@ -29,8 +29,10 @@ def draw_rectangle_with_title(img, box, disp_str, bg_color=(255, 0, 0), font_col
     draw = ImageDraw.Draw(img)
     font = ImageFont.load_default()
     font_w, font_h = font.getsize(disp_str)
-    draw.rectangle((box[0], box[1], box[0] + box[2], box[1] + box[3]), fill=None, outline=bg_color, width=2)
-    draw.rectangle((box[0], box[1] - font_h, box[0] + font_w, box[1]), fill=bg_color)
+    draw.rectangle((box[0], box[1], box[0] + box[2], box[1] +
+                   box[3]), fill=None, outline=bg_color, width=2)
+    draw.rectangle((box[0], box[1] - font_h, box[0] +
+                   font_w, box[1]), fill=bg_color)
     draw.text((box[0], box[1] - font_h), disp_str, fill=font_color, font=font)
 
 font = ImageFont.truetype("./res/baars.ttf", 40, encoding="unic")
@@ -44,8 +46,6 @@ draw.text((10, 195), u'MaixPy.Sipeed.COM', "#bdc3c7", font)
 draw.text((0, 0), u'<exit', "#7f8c8d", font)
 draw.text((160, 0), u'demo> ', "#16a085", font)
 
-
-
 exits = Image.new("RGBA", (240, 240), "#2c3e50")
 draw = ImageDraw.Draw(exits)
 draw.text((20, 40), u"Quit?\nLater can execute", "#1abc9c", font)
@@ -56,27 +56,22 @@ draw.text((0, 0), u'<No', "#2ecc71", font)
 draw.text((180, 0), u'Yes> ', "#e74c3c", font)
 # display.show(exits)
 
-
-
-
 ini = 1
 init_nn_s = 0
 
 packet = {
     'selected': "main",
-    'status' : 0
+    'status': 0
 }
 
 asyncio.set_event_loop(asyncio.new_event_loop())
-
-
 keys = InputDevice('/dev/input/event0')
 
 async def keys_events(packet, device):
     global ini
     global init_nn_s
     async for event in device.async_read_loop():
-        if event.value == 1 and event.code == 0x02:     #右键
+        if event.value == 1 and event.code == 0x02:  # 右键
             if packet["status"] == 0:
                 if packet["selected"] == "main":
                     img = Image.open("./res/face.png")
@@ -100,7 +95,7 @@ async def keys_events(packet, device):
                 display.show(canvas)
                 packet["selected"] = "main"
                 packet["status"] = 0
-        elif event.value == 1 and event.code == 0x03:#左键
+        elif event.value == 1 and event.code == 0x03:  # 左键
             if packet["status"] == 0:
                 packet["selected"] = "exit"
             else:
@@ -108,7 +103,6 @@ async def keys_events(packet, device):
                     packet["selected"] = "main"
                     packet["status"] = 0
                 else:
-
                     packet["status"] += 1
                     if packet["status"] == 5:
                         packet["status"] = 1
@@ -131,7 +125,7 @@ async def keys_events(packet, device):
                         img = Image.open("./res/qr.png")
                         img = img.convert('RGBA')
                         display.show(img)
-                        del img                        
+                        del img
                     elif packet["status"] == 5:
                         img = Image.open("./res/exit.png")
                         img = img.convert('RGBA')
@@ -143,7 +137,6 @@ for device in [keys]:
 camerax = None
 npu = None
 yolo2_decoder = None
-from maix import camera
 camerax = camera
 camera.config(size=(224, 224))
 
@@ -155,16 +148,13 @@ async def main(packet):
     global init_nn_s
     global npu
     global yolo2_decoder
-        # camera.config(size=(240,240))
+    # camera.config(size=(240,240))
     labelss = ["person"]
     anchors = [1.19, 1.98, 2.79, 4.59, 4.53, 8.92, 8.06, 5.29, 10.32, 10.65]
-    if init_nn_s == 1:                                          #分类
+    if init_nn_s == 1:  # 分类
         if npu != None:
             del npu
-            print(("-------------------------fen"))
             time.sleep(1)
-            
-
         npu = nn.load({
             "param": "./res/resnet.param",
             "bin": "./res/resnet.bin"
@@ -181,10 +171,9 @@ async def main(packet):
             "norm": [0.00784313725490196, 0.00784313725490196, 0.00784313725490196],
         })
         init_nn_s = 0
-    if init_nn_s == 2:                                  #人脸
+    if init_nn_s == 2:  # 人脸
         if npu != None:
             del npu
-            print(("-------------------------face"))
             time.sleep(1)
         model = {
             "param": "./res/yolo2_face_int8.param",
@@ -202,12 +191,10 @@ async def main(packet):
             "norm": [0.0078125, 0.0078125, 0.0078125],
         }
         npu = nn.load(model, opt=options)
-        yolo2_decoder = decoder.Yolo2(len(labelss), anchors, net_in_size=(224, 224), net_out_size=(7, 7))
+        yolo2_decoder = decoder.Yolo2(
+            len(labelss), anchors, net_in_size=(224, 224), net_out_size=(7, 7))
         init_nn_s = 0
-
-
-
-    if packet["selected"] == "demo3":                       #分类
+    if packet["selected"] == "demo3":  # 分类
         img = camerax.capture()
         if img:
             out = npu.forward(img, quantize=True)
@@ -229,7 +216,8 @@ async def main(packet):
     elif packet["selected"] == "demo1":
         img = camera.capture()
         out = npu.forward(img, quantize=True, layout="hwc")
-        boxes, probs = yolo2_decoder.run(out, nms=0.3, threshold=0.5, img_size=(224, 224))
+        boxes, probs = yolo2_decoder.run(
+            out, nms=0.3, threshold=0.5, img_size=(224, 224))
         if len(boxes):
             t = time.time()
             img = Image.new("RGBA", (240, 240), "#00000000")
@@ -239,48 +227,46 @@ async def main(packet):
                 disp_str = "{}:{:.2f}%".format(labelss[class_id], prob*100)
                 draw_rectangle_with_title(img, box, disp_str)
             display.show(img)
-            print("-- draw: ", time.time() - t )
+            print("-- draw: ", time.time() - t)
             t = time.time()
             # display.show(img)
-            print("-- show: ", time.time() - t )
+            print("-- show: ", time.time() - t)
         else:
             display.show(Image.new("RGBA", (240, 240), "#00000000"))
 
-
-    elif packet["selected"] == "demo2": 
-        img = camerax.read() # 224 * 224
-        img = camerax.camera.cam.get_vi() # 240 * 240
+    elif packet["selected"] == "demo2":
+        img = camerax.read()  # 224 * 224
+        img = camerax.camera.cam.get_vi()  # 240 * 240
         if len(img):
-            image.send_to_image(img,240,240)
-            ma = image.find_blobs([(2, 39, 11, 63, 7, 36)],roi=[], x_stride=1, y_stride=1, invert=False, area_threshold=1, pixels_threshold=20, merge=True, margin=0)
+            image.send_to_image(img, 240, 240)
+            ma = image.find_blobs([(2, 39, 11, 63, 7, 36)], roi=[], x_stride=1, y_stride=1,
+                                  invert=False, area_threshold=1, pixels_threshold=20, merge=True, margin=0)
             for i in ma:
-                image.draw_rectangle(i["x"],i["y"],i["w"],i["h"])
+                image.draw_rectangle(i["x"], i["y"], i["w"], i["h"])
             # img_a = image.img_back_rgb888()
-            img = Image.frombytes(mode="RGB", size=(240,240), data=image.img_back_rgb888(), decoder_name="raw")
+            img = Image.frombytes(mode="RGB", size=(
+                240, 240), data=image.img_back_rgb888(), decoder_name="raw")
             image.close()
             display.show(img)
-    elif packet["selected"] == "demo4":                     #二维码
+    elif packet["selected"] == "demo4":  # 二维码
         img = camerax.capture()
         codes = zbarlight.scan_codes(['qrcode'], img)
         if codes:
             print(codes)
             images = Image.new("RGBA", (240, 240), "#00000000")
             draw = ImageDraw.Draw(images)
-            draw.text((10, 10), codes[0].decode('ascii'), (255, 0, 0), font) # bgr
+            draw.text((10, 10), codes[0].decode(
+                'ascii'), (255, 0, 0), font)  # bgr
             display.show(images)
-
         else:
             images = Image.new("RGBA", (240, 240), "#00000000")
             draw = ImageDraw.Draw(images)
-            draw.text((10, 10), "not qr", (255, 0, 0), font) # bgr
+            draw.text((10, 10), "not qr", (255, 0, 0), font)  # bgr
             display.show(images)
     # else:
     #     # await asyncio.sleep(0.02)
     asyncio.ensure_future(main(packet))
 
 asyncio.ensure_future(main(packet))
-
 loop = asyncio.get_event_loop()
 loop.run_forever()
-
-
